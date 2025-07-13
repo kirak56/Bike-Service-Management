54ce1ab121469aac1cafcb15bbe58f0bc21a26da
kiran <kirankng67890@gmail.com>
2025-07-13 20:33:40 +0530

    formating the global exception page

--- src/main/java/com/tesla/bikeservices/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tesla/bikeservices/exception/GlobalExceptionHandler.java
@@ -15,59 +15,60 @@ import jakarta.persistence.EntityNotFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-	
+
 	private static final Logger logger = LogManager.getLogger(GlobalExceptionHandler.class);
+
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-		
+
 		Map<String, String> errors = new HashMap<>();
 		for (FieldError error : ex.getBindingResult().getFieldErrors()) {
-			
-            errors.put(error.getField(), error.getDefaultMessage());
-            logger.error("Validation failed for field {}: {}", error.getField(), error.getDefaultMessage());
-        }
-		
+
+			errors.put(error.getField(), error.getDefaultMessage());
+			logger.error("Validation failed for field {}: {}", error.getField(), error.getDefaultMessage());
+		}
+
 		Map<String, Object> errorResponse = new HashMap<>();
-        errorResponse.put("timestamp", LocalDateTime.now());
-        errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
-        errorResponse.put("error", "Bad Request");
-        errorResponse.put("message", "Validation failed");
-        errorResponse.put("errors", errors);
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+		errorResponse.put("timestamp", LocalDateTime.now());
+		errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
+		errorResponse.put("error", "Bad Request");
+		errorResponse.put("message", "Validation failed");
+		errorResponse.put("errors", errors);
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(EntityNotFoundException.class)
 	public ResponseEntity<Map<String, Object>> handleEntityNotFoundException(EntityNotFoundException ex) {
 		logger.error("Unexpected error occurred: {} at request: {}", ex.getMessage(), ex);
-        Map<String, Object> errorResponse = new HashMap<>();
-        errorResponse.put("timestamp", LocalDateTime.now());
-        errorResponse.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-        errorResponse.put("error", "Internal Server Error");
-        errorResponse.put("message", "An unexpected error occurred");
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+		Map<String, Object> errorResponse = new HashMap<>();
+		errorResponse.put("timestamp", LocalDateTime.now());
+		errorResponse.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		errorResponse.put("error", "Internal Server Error");
+		errorResponse.put("message", "An unexpected error occurred");
+		return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
 	}
 
 	@ExceptionHandler(IllegalStateException.class)
-	public ResponseEntity<Map<String, Object>>  handleIllegalStateException(IllegalStateException ex) {
+	public ResponseEntity<Map<String, Object>> handleIllegalStateException(IllegalStateException ex) {
 		logger.error("Unexpected error occurred: {} at request: {}", ex.getMessage(), ex);
-        Map<String, Object> errorResponse = new HashMap<>();
-        errorResponse.put("timestamp", LocalDateTime.now());
-        errorResponse.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-        errorResponse.put("error", "Internal Server Error");
-        errorResponse.put("message", "An unexpected error occurred");
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+		Map<String, Object> errorResponse = new HashMap<>();
+		errorResponse.put("timestamp", LocalDateTime.now());
+		errorResponse.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		errorResponse.put("error", "Internal Server Error");
+		errorResponse.put("message", "An unexpected error occurred");
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<Map<String, Object>> handleGeneralException(Exception ex) {
 		logger.error("Unexpected error occurred: {} at request: {}", ex.getMessage(), ex);
 
-        Map<String, Object> errorResponse = new HashMap<>();
-        errorResponse.put("timestamp", LocalDateTime.now());
-        errorResponse.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-        errorResponse.put("error", "Internal Server Error");
-        errorResponse.put("message", "An unexpected error occurred");
+		Map<String, Object> errorResponse = new HashMap<>();
+		errorResponse.put("timestamp", LocalDateTime.now());
+		errorResponse.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		errorResponse.put("error", "Internal Server Error");
+		errorResponse.put("message", "An unexpected error occurred");
 
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);		
+		return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
